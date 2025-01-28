7aba2b5e6b0f7134ad4a978b430130077daad37b
idera nimah <iderayusuf29@gmail.com>
2025-01-28 11:47:20 +0100

    fourth

--- src/Components/PageFour/PageFour.jsx
+++ src/Components/PageFour/PageFour.jsx
@@ -1,10 +1,17 @@
 import "./PageFour.css";
+import simpattern from "../../assets/bg-simplify-section-mobile.svg";
+
 
 const PageFour = () => {
   return (
     <div className="page-four">
       <h1>Simplify how your team works today.</h1>
       <button className="pagefourbtn">Get Started</button>
+      <img
+                src={simpattern}
+                alt="tabletpattern"
+                className="pagefourillustration"
+              />
     </div>
   );
 };
